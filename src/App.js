284d64c5b791ad39ffc0c9d5c556678c895a491a
LatoyaNatashaC <Lnatashacampbell@gmail.com>
2025-09-22 16:00:10 -0700

    Verify Bootstrap CDN integration is working

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <a href="#" className="btn btn-primary">
+        Search
+      </a>
+    </div>
+  );
 }
 
 export default App;
