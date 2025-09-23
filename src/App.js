a032448fb7d7bfee7d0d802a4ffca6eb6ffb2ef0
LatoyaNatashaC <Lnatashacampbell@gmail.com>
2025-09-22 17:16:40 -0700

    Verify Google Fonts CDN integration is working

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <a href="#" className="btn btn-primary">
-        Search
-      </a>
+      <h1>shubh din</h1>
+      <h2>shubh sandhya</h2>
+      <h3>shubh raatri</h3>
     </div>
   );
 }
