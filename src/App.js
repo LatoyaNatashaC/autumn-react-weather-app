c7f1f5f5b169ca2a9d62190f16a49ae4a4044f5d
LatoyaNatashaC <Lnatashacampbell@gmail.com>
2025-09-26 13:10:07 -0700

    Remove code/files not relevant to project

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>shubh din</h1>
-      <h2>shubh sandhya</h2>
-      <h3>shubh raatri</h3>
-    </div>
-  );
+  return <div className="App">Autumn Weather App</div>;
 }
 
 export default App;
