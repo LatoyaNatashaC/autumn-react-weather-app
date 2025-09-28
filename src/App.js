8d41cb1780bca8543825f44b62501018a71d7006
LatoyaNatashaC <Lnatashacampbell@gmail.com>
2025-09-28 09:29:38 -0700

    Create component to structure Forecast UI layout

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import Weather from "./Weather";
+import Forecast from "./Forecast";
 
 export default function App() {
   return (
@@ -8,6 +9,7 @@ export default function App() {
       <div className="container">
         <h1>Weather Forecast</h1>
         <Weather />
+        <Forecast />
       </div>
     </div>
   );
