cbbd8f679c445f7e241100cd3546a11208402f8c
LatoyaNatashaC <Lnatashacampbell@gmail.com>
2025-10-01 10:01:39 -0700

    Edit <select> element default label

--- src/Theme.js
+++ src/Theme.js
@@ -3,8 +3,8 @@ import React from "react";
 export default function Theme() {
   return (
     <div className="selector-box">
-      <select id="theme">
-        <option value="">Choose a season to change the theme</option>
+      <select id="seasonSelect">
+        <option value="">Choose a theme...</option>
         <option value="Spring">Spring</option>
         <option value="Summer">Summer</option>
         <option value="Autumn">Autumn</option>
