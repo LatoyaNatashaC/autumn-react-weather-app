6c1ccc5741b7af132c727ca8459ec76d39f9c0fc
LatoyaNatashaC <Lnatashacampbell@gmail.com>
2025-10-12 16:25:51 -0700

    Refactor App.js to hold theme state and pass props to Theme.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,43 @@
 import React, { useState } from "react";
+
 import "./App.css";
 import Weather from "./Weather";
 import Forecast from "./Forecast";
 import Theme from "./Theme";
 import summerImg from "./images/summer.png";
 import springImg from "./images/spring.png";
-import autumnImg from "./images/fall.png";
+import autumnImg from "./images/autumn.png";
 import winterImg from "./images/winter.png";
 
 export default function App() {
+  const seasonThemes = {
+    summer: summerImg,
+    spring: springImg,
+    autumn: autumnImg,
+    winter: winterImg,
+  };
+  const [season, setSeason] = useState("autumn");
+
+  function handleSelection(event) {
+    const selectedSeason = event.target.value;
+    console.log("User selected:", selectedSeason);
+    if (selectedSeason in seasonThemes) {
+      setSeason(selectedSeason);
+    }
+  }
   return (
-    <div className="App">
+    <div
+      className="App"
+      style={{
+        backgroundImage: `url(${seasonThemes[season]})`,
+        backgroundSize: "cover",
+        minHeight: "100vh",
+        padding: "30px",
+        transition: "background-image 0.4s ease-in-out",
+      }}
+    >
       <div className="container">
-        <Theme />
+        <Theme season={season} handleSelection={handleSelection} />
         <h1 className="mb-3">Weather Forecast</h1>
 
         <Weather />
