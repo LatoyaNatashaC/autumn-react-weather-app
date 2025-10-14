bc6ff3034502b81a4aad7a67c1e59cf5d1ea2d9f
LatoyaNatashaC <Lnatashacampbell@gmail.com>
2025-10-14 15:47:35 -0700

    Refactor Weather.js with useEffect to avoid Axios 429 error during build

--- src/Weather.js
+++ src/Weather.js
@@ -1,9 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./Weather.css";
 import axios from "axios";
 
 export default function Weather() {
   const [weatherData, setWeatherData] = useState({ ready: false });
+
+  const useMockData = true;
 
   function handleResponse(response) {
     console.log(response.data);
@@ -11,7 +13,7 @@ export default function Weather() {
       ready: true,
       city: response.data.name,
       temperature: response.data.main.temp,
-      iconurl:
+      iconUrl:
         "https://www.gstatic.com/weather/conditions2023/2023.2/svg/drizzle_light.svg",
       description: response.data.weather[0].description,
       humidity: response.data.main.humidity,
@@ -19,51 +21,68 @@ export default function Weather() {
     });
   }
 
-  if (weatherData.ready) {
-    return (
-      <div className="weather">
-        <form>
-          <div className="row">
-            <div className="col-9">
-              <input
-                type="search"
-                placeholder="Enter city"
-                className="form-control"
-              />
-            </div>
-            <div className="col-3">
-              <input type="submit" value="Search" className="btn btn-primary" />
-            </div>
-          </div>
-        </form>
-        <h2 className="mt-3">Los Angeles</h2>
-        <ul>
-          <li>Tuesday 14</li>
-          <li>{weatherData.description}</li>
-        </ul>
-        <div className="row">
-          <div className="col-6">
-            <img src={weatherData.iconUrl} alt={weatherData.description} />
-            <span className="currentTemperature">
-              {Math.round(weatherData.temperature)}
-            </span>{" "}
-            <span className="unit">ºF</span>
-          </div>
-          <div className="col-6">
-            <ul>
-              <li>Precepitation: 0% </li>
-              <li>Humidity:{weatherData.humidity}%</li>
-              <li>Wind:{weatherData.wind} mph</li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    );
-  } else {
-    const apiKey = "203fa770242fcd2b9555d832a88ea567";
-    let city = "Los Angeles";
-    let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
-    axios.get(apiUrl).then(handleResponse);
+  function getMockWeatherData() {
+    return {
+      city: "Test City",
+      main: { temp: 59, humidity: 77 },
+      weather: [{ description: "Light Rain" }],
+      wind: { speed: 5 },
+    };
+  }
+
+  useEffect(() => {
+    if (useMockData) {
+      setTimeout(() => {
+        handleResponse({ data: getMockWeatherData() });
+      }, 500);
+    } else {
+      const apiKey = "203fa770242fcd2b9555d832a88ea567";
+      let city = "Los Angeles";
+      let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+      axios.get(apiUrl).then(handleResponse);
+    }
+  }, []);
+
+  if (!weatherData.ready) {
     return "Loading...";
   }
+  return (
+    <div className="weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter city"
+              className="form-control"
+            />
+          </div>
+          <div className="col-3">
+            <input type="submit" value="Search" className="btn btn-primary" />
+          </div>
+        </div>
+      </form>
+      <h2 className="mt-3">{weatherData.city}</h2>
+      <ul>
+        <li>Tuesday 14</li>
+        <li>{weatherData.description}</li>
+      </ul>
+      <div className="row">
+        <div className="col-6">
+          <img src={weatherData.iconUrl} alt={weatherData.description} />
+          <span className="currentTemperature">
+            {Math.round(weatherData.temperature)}
+          </span>{" "}
+          <span className="unit">ºF</span>
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Precepitation: 0% </li>
+            <li>Humidity:{weatherData.humidity}%</li>
+            <li>Wind:{weatherData.wind} mph</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
