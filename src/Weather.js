1562eb450407e0e0e836c7248675af9e06a7602d
LatoyaNatashaC <Lnatashacampbell@gmail.com>
2025-10-27 16:45:26 -0700

    Remove mock weather data and use live API for weather display

--- src/Weather.js
+++ src/Weather.js
@@ -1,88 +1,71 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "./Weather.css";
 import axios from "axios";
 
 export default function Weather() {
   const [weatherData, setWeatherData] = useState({ ready: false });
 
-  const useMockData = true;
-
   function handleResponse(response) {
     console.log(response.data);
+
     setWeatherData({
       ready: true,
       city: response.data.name,
+      date: "October 27th",
       temperature: response.data.main.temp,
       iconUrl:
-        "https://www.gstatic.com/weather/conditions2023/2023.2/svg/drizzle_light.svg",
+        "https://www.gstatic.com/weather/conditions/v1/svg/sunny_light.svg",
       description: response.data.weather[0].description,
       humidity: response.data.main.humidity,
       wind: response.data.wind.speed,
     });
   }
 
-  function getMockWeatherData() {
-    return {
-      city: "Test City",
-      main: { temp: 59, humidity: 77 },
-      weather: [{ description: "Light Rain" }],
-      wind: { speed: 5 },
-    };
-  }
-
-  useEffect(() => {
-    if (useMockData) {
-      setTimeout(() => {
-        handleResponse({ data: getMockWeatherData() });
-      }, 500);
-    } else {
-      const apiKey = "203fa770242fcd2b9555d832a88ea567";
-      let city = "Los Angeles";
-      let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
-      axios.get(apiUrl).then(handleResponse);
-    }
-  }, []);
-
-  if (!weatherData.ready) {
-    return "Loading...";
-  }
-  return (
-    <div className="weather">
-      <form>
+  if (weatherData.ready) {
+    return (
+      <div className="weather">
+        <form>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Enter city"
+                className="form-control"
+              />
+            </div>
+            <div className="col-3">
+              <input type="submit" value="Search" className="btn btn-primary" />
+            </div>
+          </div>
+        </form>
+        <h2 className="mt-3">{weatherData.city}</h2>
+        <ul>
+          <li>{weatherData.date}</li>
+          <li>{weatherData.description}</li>
+        </ul>
         <div className="row">
-          <div className="col-9">
-            <input
-              type="search"
-              placeholder="Enter city"
-              className="form-control"
-            />
+          <div className="col-6">
+            <img src={weatherData.iconUrl} alt={weatherData.description} />
+            <span className="currentTemperature">
+              {Math.round(weatherData.temperature)}
+            </span>{" "}
+            <span className="unit">ºF</span>
           </div>
-          <div className="col-3">
-            <input type="submit" value="Search" className="btn btn-primary" />
+          <div className="col-6">
+            <ul>
+              <li>Humidity:{weatherData.humidity}%</li>
+              <li>Wind:{weatherData.wind} mph</li>
+            </ul>
           </div>
-        </div>
-      </form>
-      <h2 className="mt-3">{weatherData.city}</h2>
-      <ul>
-        <li>Tuesday 14</li>
-        <li>{weatherData.description}</li>
-      </ul>
-      <div className="row">
-        <div className="col-6">
-          <img src={weatherData.iconUrl} alt={weatherData.description} />
-          <span className="currentTemperature">
-            {Math.round(weatherData.temperature)}
-          </span>{" "}
-          <span className="unit">ºF</span>
-        </div>
-        <div className="col-6">
-          <ul>
-            <li>Precepitation: 0% </li>
-            <li>Humidity:{weatherData.humidity}%</li>
-            <li>Wind:{weatherData.wind} mph</li>
-          </ul>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    const apiKey = "203fa770242fcd2b9555d832a88ea567";
+    let city = "Los Angeles";
+    let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+    axios.get(apiUrl).then(handleResponse);
+
+    return "Loading Weather...";
+  }
 }
