0d565e8f8dda3856cd3e3f9b6ffc57e3331205b1
LatoyaNatashaC <Lnatashacampbell@gmail.com>
2025-11-05 20:27:09 -0800

    Enable icon update on city search

--- src/Weather.js
+++ src/Weather.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./Weather.css";
 import axios from "axios";
 import DisplayDate from "./DisplayDate";
@@ -6,6 +6,10 @@ import DisplayDate from "./DisplayDate";
 export default function Weather() {
   const [weatherData, setWeatherData] = useState({ ready: false });
   const [city, setCity] = useState("Los Angeles");
+
+  useEffect(() => {
+    search();
+  }, []);
 
   function handleResponse(response) {
     console.log(response.data);
@@ -15,8 +19,7 @@ export default function Weather() {
       city: response.data.name,
       date: new Date(response.data.dt * 1000),
       temperature: response.data.main.temp,
-      iconUrl:
-        "https://www.gstatic.com/weather/conditions/v1/svg/sunny_light.svg",
+      iconUrl: `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
       description: response.data.weather[0].description,
       humidity: response.data.main.humidity,
       wind: response.data.wind.speed,
@@ -25,7 +28,7 @@ export default function Weather() {
 
   function search() {
     const apiKey = "203fa770242fcd2b9555d832a88ea567";
-    let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
     axios.get(apiUrl).then(handleResponse);
   }
 
@@ -38,50 +41,54 @@ export default function Weather() {
     setCity(event.target.value);
   }
 
-  if (weatherData.ready) {
-    return (
-      <div className="weather">
-        <form onSubmit={handleSearch}>
-          <div className="row">
-            <div className="col-9">
-              <input
-                type="search"
-                placeholder="Enter city"
-                className="form-control"
-                onChange={handleCityChange}
-              />
-            </div>
-            <div className="col-3">
-              <input type="submit" value="Search" className="btn btn-primary" />
-            </div>
-          </div>
-        </form>
-        <h2 className="mt-3">{weatherData.city}</h2>
-        <ul>
-          <li>
-            <DisplayDate date={weatherData.date} />
-          </li>
-          <li>{weatherData.description}</li>
-        </ul>
-        <div className="row">
-          <div className="col-6">
-            <img src={weatherData.iconUrl} alt={weatherData.description} />
-            <span className="currentTemperature">
-              {Math.round(weatherData.temperature)}
-            </span>{" "}
-            <span className="unit">ºF</span>
-          </div>
-          <div className="col-6">
-            <ul>
-              <li>Humidity:{weatherData.humidity}%</li>
-              <li>Wind:{weatherData.wind} mph</li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    );
-  } else {
-    search();
+  if (!weatherData.ready) {
     return "Loading Weather...";
   }
+
+  return (
+    <div className="weather">
+      <form onSubmit={handleSearch}>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter city"
+              className="form-control"
+              onChange={handleCityChange}
+            />
+          </div>
+          <div className="col-3">
+            <input type="submit" value="Search" className="btn btn-primary" />
+          </div>
+        </div>
+      </form>
+      <h2 className="mt-3">{weatherData.city}</h2>
+      <ul>
+        <li>
+          <DisplayDate date={weatherData.date} />
+        </li>
+        <li>{weatherData.description}</li>
+      </ul>
+      <div className="row">
+        <div className="col-6">
+          <div className="clearfix">
+            <div className="float-left">
+              <img src={weatherData.iconUrl} alt={weatherData.description} />
+              <span className="currentTemperature">
+                {Math.round(weatherData.temperature)}
+              </span>{" "}
+              <span className="unit">ºF</span>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-6">
+          <ul>
+            <li>Humidity:{weatherData.humidity}%</li>
+            <li>Wind:{weatherData.wind} mph</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
