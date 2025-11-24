6cde85769c95441d07a9aedeaf1ec0032e93ac82
LatoyaNatashaC <Lnatashacampbell@gmail.com>
2025-11-24 01:34:31 -0800

    Create function for temperature unit conversion

---
+++ src/CurrentWeather.js
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+
+export default function CurrentWeather(props) {
+  const [unit, setUnit] = useState("fahrenheit");
+
+  function displayCelsius(event) {
+    event.preventDefault();
+    setUnit("celsius");
+  }
+  function displayFahrenheit(event) {
+    event.preventDefault();
+    setUnit("fahrenheit");
+  }
+  if (unit === "fahrenheit") {
+    return (
+      <div className="currentWeather">
+        <span className="currentTemperature">
+          {Math.round(props.fahrenheit)}
+        </span>
+        <span className="unit">
+          ℉ |{" "}
+          <a href="/" onClick={displayCelsius}>
+            ℃
+          </a>
+        </span>
+      </div>
+    );
+  } else {
+    let celsius = ((props.fahrenheit - 32) * 5) / 9;
+    return (
+      <div className="currentWeather">
+        <span className="currentTemperature">{Math.round(celsius)}</span>{" "}
+        <span className="unit">
+          <a href="/" onClick={displayFahrenheit}>
+            ℉
+          </a>{" "}
+          | ℃
+        </span>
+      </div>
+    );
+  }
+}
